83df4771ab4583a7ab9fdc5c17b07648060a951b
shahabdadkhan <shahabkhan@fynd-external.com>
2022-06-26 12:02:29 +0530

    completed store UI

--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -1,3 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+import { StoreItem } from "../components/storeItem";
+import storeItems from "../data/item.json"
+
 export function Store () {
- return <h1>What are you doing in the store?</h1>
+ return( <>
+    <h1>Store</h1>
+    <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map(item=>( 
+            // <Col>
+            //     Hi Bro
+            // </Col>
+            <Col key={item.id}>
+                {/* {JSON.stringify(item)} */}
+                <StoreItem {...item} />
+                </Col>
+        ))}
+    </Row>
+ </>)
 }
